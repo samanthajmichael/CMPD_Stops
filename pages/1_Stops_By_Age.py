7a22961459f9bf8ffb74c011b8061605080fe194
samanthajmichael <samanthajmichael@gmail.com>
2024-04-08 19:04:44 -0400

    Added pages

---
+++ pages/1_Stops_By_Age.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+st.write('CMPD Traffic Stops by Age')
+
+@st.cache_data
+def load_data(csv):
+    df= pd.read_csv(csv)
+    return df
+
+stops = load_data('data/Officer_Traffic_Stops.csv')
+
+## Histogram
+age_hist = sns.histplot(stops, x='Driver_Age', kde=True, bins=20)
+age_hist.set_title('Distribution of Driver Age (bins: 20)')
+age_hist.set_xlabel('Driver Age in Years')
+age_hist.set_xticks(range(0,100, 5))
+
+st.pyplot(age_hist.get_figure())
