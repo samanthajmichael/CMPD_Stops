276dbf1ba9d84eaaa72477fb98ebc50e317fc4a2
samanthajmichael <samanthajmichael@gmail.com>
2024-04-09 10:31:01 -0400

    Added altair chart

--- pages/2_Was_a_Search_Conducted.py
+++ pages/2_Was_a_Search_Conducted.py
@@ -19,3 +19,37 @@ age_box.set_xlabel('Was a Search Conducted: Y/N')
 age_box.set_ylabel('Age of Driver in Years')
 
 st.pyplot(age_box.get_figure())
+
+# Calculate the Search counts for yes and no
+search_counts = stops['Was_a_Search_Conducted'].value_counts().reset_index()
+
+# Rename the columns
+search_counts.columns = ['Was_a_Search_Conducted', 'count']
+
+# Altair Bar Chart
+alt_search = (
+    alt.Chart(search_counts, title='Bar Chart of Searches Conducted')
+    .mark_bar()
+    .encode(
+        x=alt.X('Was_a_Search_Conducted', title='Search Conducted'),
+        y=alt.Y('count', title='Number of Searches'),
+        color=alt.Color('Was_a_Search_Conducted', title='Search Conducted'),
+        tooltip=[
+            alt.Tooltip('Was_a_Search_Conducted', title='Search Conducted:'),
+            alt.Tooltip('count', title='Count:')]
+    )
+    .properties(width=400)
+)
+
+# Add text labels
+alt_search_text = alt_search.mark_text(
+    align='center',
+    baseline='bottom',
+    dx=0,
+    dy=-5
+).encode(
+    text=alt.Text('count:Q')
+)
+
+alt_search_combined = alt.layer(alt_search, alt_search_text)
+st.altair_chart(alt_search_combined)
